af2617c1ce9b7df6c36efd9f3acc2a9ad9315dde
valuybruno <valuybruno@192.168.0.163>
2021-04-10 18:09:54 -0300

    lalala

--- src/pb2_clase1/Calculadora.java
+++ src/pb2_clase1/Calculadora.java
@@ -7,7 +7,7 @@ public class Calculadora {
 		 Integer a;
 		 Integer b;
 		 Integer opcion;
-		 Integer resultado;
+		 Integer resultado = 0;
 		Scanner teclado= new Scanner(System.in);
 		 do{
 		 System.out.println("Ingrese su operación \n 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir");
@@ -17,14 +17,25 @@ public class Calculadora {
 		 a = teclado.nextInt();
 		 System.out.println("Ingrese el segundo numero");
 		 b = teclado.nextInt();
-		 if(opcion.equals(1)){
-		 resultado = a + b;
-		 }else if(opcion.equals(2)){
-		 resultado = a - b;
-		 }else if(opcion.equals(3)){
-		 resultado = a * b;
-		 }else{
-		 resultado = a / b;
+ switch (opcion) {
+		 
+		 case 1: resultado = a + b;
+		 
+		 break;
+		 
+		 case 2:resultado = a - b;
+		
+		 break;
+		 
+		 case 3: resultado = a * b;
+		 
+		 break;
+		 
+		 case 4: resultado = a / b;
+		 
+		 break;
+		 
+		 default: System.out.println("ERROR");
 		 }
 		 System.out.println("El resultado es " + resultado.toString());
 	
